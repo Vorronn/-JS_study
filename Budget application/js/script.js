4ced975674364ac8434bcae338a175dacc1d64bb
Siarhey Sapelko <sergeysapelko90@gmail.com>
2020-04-29 00:23:41 +0300

    the addition of loops and conditions

--- Budget application/js/script.js
+++ Budget application/js/script.js
@@ -1,5 +1,4 @@
-'use strict'
-let money = prompt("Ваш бюджет на месяц", ''),
+let money = +prompt("Ваш бюджет на месяц", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -11,12 +10,27 @@ let appData = {
     savings: false
 };
 
-let questionOne = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    questionTwo = prompt("Во сколько обойдется?", ''),
-    questionThree = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    questionFour = prompt("Во сколько обойдется?", '');
+for (let i=0; i<2; i++){
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
 
-    appData.expenses.questionOne = questionTwo;
-    appData.expenses.questionThree = questionFour;
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
 
-alert(appData.budget/30);
+}
+
+appData.moneyPerDay = appData.budget / 30;
+alert(appData.moneyPerDay);
+if (appData.moneyPerDay < 100) {
+    console.log('Низкий уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 300) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 300) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
+console.log(appData);
