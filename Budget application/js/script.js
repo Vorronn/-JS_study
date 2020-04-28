6a74f9445b19c83dc78f440acac5c1d127642643
Siarhey Sapelko <sergeysapelko90@gmail.com>
2020-04-28 22:30:55 +0300

    start project budget

---
+++ Budget application/js/script.js
@@ -0,0 +1,22 @@
+'use strict'
+let money = prompt("Ваш бюджет на месяц", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let questionOne = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    questionTwo = prompt("Во сколько обойдется?", ''),
+    questionThree = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    questionFour = prompt("Во сколько обойдется?", '');
+
+    appData.expenses.questionOne = questionTwo;
+    appData.expenses.questionThree = questionFour;
+
+alert(appData.budget/30);
