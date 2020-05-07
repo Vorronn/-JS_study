bf50ebd196cabfa666c2c5651365adcaee7a9777
Siarhey Sapelko <sergeysapelko90@gmail.com>
2020-05-07 20:27:08 +0300

    get element and reverce style with javaScript

--- GetElement/js/script.js
+++ GetElement/js/script.js
@@ -2,10 +2,39 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.wrapper .heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle[2]);
-console.log(heart[1]);
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '50%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++){
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function(item, i, hearts){
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Hello world');
+
+div.classList.add('black');
+
+// div.innerHTML = '<h1>Hello world!!!</h1>';
+div.textContent = 'Hello World!';
+
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
+
+console.log(div);
